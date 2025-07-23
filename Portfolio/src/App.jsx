03e354b0348fde69d953940bc9c4b924205ca896
Satendra Singh <iamsatndra@gmail.com>
2025-07-23 08:14:13 +0530

    About section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import styles from "./App.module.css";
 import { Navbar } from './Components/Navbar/Navbar';
 import { Hero } from './Components/Hero/Hero';
+import { About } from './Components/About/About';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className={styles.App}>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   )
 }
